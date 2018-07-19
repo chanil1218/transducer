3e1a9861d4b17c9887bfeac9cbf2e33633d68e35
Chanil <chanil1218@gmail.com>
2018-07-19 16:04:20 +0900

    Make pip installable via git

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import torch
+from setuptools import setup, find_packages
+from torch.utils.ffi import create_extension
+
+this_file = os.path.abspath(__file__)
+
+sources = ['src/transducer.c']
+headers = ['src/transducer.h']
+
+args = ["-std=c99"]
+if sys.platform == "darwin":
+    args += ["-DAPPLE"]
+else:
+    args += ["-fopenmp"]
+
+ffi = create_extension(
+    "_ext.transducer",
+    package=True,
+    headers=headers,
+    sources=sources,
+    extra_compile_args=args
+)
+ffi = ffi.distutils_extension()
+
+setup(
+    name="transducer",
+    version="1.0",
+    description="A Fast Sequence Transducer Implementation with PyTorch Bindings",
+    url="https://github.com/awni/transducer",
+    author="Awni Hannun",
+    license="Apache",
+    packages=find_packages(),
+    ext_modules=[ffi],
+)
